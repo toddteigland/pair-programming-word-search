eac8e34935c4864271211d2190ae0acab439cba6
toddteigland <toddteigland@gmail.com>
2022-11-23 20:08:05 -0800

    Edited the code to properly search vertically, as well as passing other tests

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) {
+      return undefined;
     }
-}
-
-module.exports = wordSearch
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (l of horizontalJoin) {
+      if (l.includes(word)) return true;
+      
+    }
+    const vetrcialJoin = fun(letters).map(ls => ls.join(''));
+    for (l of vetrcialJoin) {
+      if (l.includes(word)) return true;
+      
+      
+    }
+    return false;
+  };
+  
+  const fun = (letters) => {
+    
+    let arr = [];
+  
+    for (let i = 0; i < letters[0].length; i++) {
+      arr.push([]);
+      for (let j = 0; j < letters.length; j++) {
+        arr[i][j] = letters[j][i];
+      }
+      return letters;
+    }
+  };
+  
+   
+  
+  module.exports = wordSearch;
+  
